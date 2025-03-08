c6a66af9a202eee3bc10dedbc078a0037f5d3845
laqylindsie <laqylindsie@gmx.com>
2025-03-08 20:35:33 +0000

    Add 5.py

---
+++ src/5.py
@@ -0,0 +1,5 @@
+import random
+
+def get_random_code(length):
+    alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    return ''.join(random.choice(alphabet) for i in range(length))
